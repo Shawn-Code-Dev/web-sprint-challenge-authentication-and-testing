4ac97597b16b47542e39581492ff84b41526bc3d
Shawn Rice <Shawn.CS@protonmail.com>
2021-10-15 03:39:29 -0400

    test with proper token

--- api/server.test.js
+++ api/server.test.js
@@ -53,6 +53,23 @@ describe('server.js', () => {
       const res = await request(server).get('/api/jokes').set('Authorization', 'garbage')
       expect(res.body.message).toMatch(/token invalid/i)
     }, 750)
+    it('[7] requests with a valid token obtains jokes', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'Shawn', password: '1234' })
+      let res = await request(server).post('/api/auth/login').send({ username: 'Shawn', password: '1234' })
+      res = await request(server).get('/api/jokes').set('Authorization', res.body.token)
+      expect(res.body).toMatchObject([{
+        "id": "0189hNRf2g",
+        "joke": "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later."
+      },
+      {
+        "id": "08EQZ8EQukb",
+        "joke": "Did you hear about the guy whose whole left side was cut off? He's all right now."
+      },
+      {
+        "id": "08xHQCdx5Ed",
+        "joke": "Why didn’t the skeleton cross the road? Because he had no guts."
+      }])
+    }, 750)
   })
   
 })
