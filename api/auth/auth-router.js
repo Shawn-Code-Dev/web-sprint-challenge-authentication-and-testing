9fc7d9128bfb403eb8a966066336bf987454e6c2
Shawn Rice <Shawn.CS@protonmail.com>
2021-10-15 03:01:06 -0400

    2 tests per endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -32,7 +32,7 @@ router.post('/login', validatePayload, (req, res, next) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = tokenBuilder(user)
         res.status(200).json({
-          message: `${username} is back!`,
+          message: `welcome, ${username}`,
           token
         })
       } else {
@@ -43,29 +43,6 @@ router.post('/login', validatePayload, (req, res, next) => {
       }
     }) 
     .catch(next)
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
 });
 
 module.exports = router;
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,58 @@
-// Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+const bcrypt = require('bcryptjs')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
 test('sanity', () => {
   expect(true).toBe(true)
+})
+
+describe('server.js', () => {
+  describe('[POST] /api/auth/register', () => {
+    it('[1] creates a new user in the database', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'Shawn', password: '1234' })
+      const shawn = await db('users').where('username', 'Shawn').first()
+      expect(shawn).toMatchObject({ username: 'Shawn' })
+    }, 750)
+    it('[2] saves the hashed password to database', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'Shawn', password: '1234' })
+      const shawn = await db('users').where('username', 'Shawn').first()
+      expect(bcrypt.compareSync('1234', shawn.password)).toBeTruthy()
+    }, 750)
+  })
+
+  describe('[POST] /api/auth/login', () => {
+    it('[3] successful login responds with correct res', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'Shawn', password: '1234' })
+      const res = await request(server).post('/api/auth/login').send({ username: 'Shawn', password: '1234' })
+      expect(res.body.message).toMatch(/welcome, Shawn/i)
+      expect(res.status).toBe(200)
+    }, 750)
+    it('[4] rejects invalid credentials', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'Shawn', password: '1234' })
+      const res = await request(server).post('/api/auth/login').send({ username: 'Shawn', password: '4321' })
+      expect(res.body.message).toMatch(/invalid credentials/i)
+      expect(res.status).toBe(401)
+    }, 750)
+  })
+
+  describe('[GET] /api/jokes', async () => {
+    it('[5] requests without a token are bounced', async () => {
+      const res = await request(server).get('/api/jokes')
+      expect(res.body.message).toMatch(/token required/i)
+    }, 750)
+    it('[6] requests with an invalid token are bounced', async () => {
+      const res = await request(server).get('/api/jokes').set('Authorization', 'garbage')
+      expect(res.body.message).toMatch(/token invalid/i)
+    }, 750)
+  })
+  
 })
